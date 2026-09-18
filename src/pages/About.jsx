968b980ff27f440a9a6490f29a90d26aa5839964
MD. Fazle Rabbi Riyad <135745417+RiyadSheikh27@users.noreply.github.com>
2026-09-19 00:17:44 +0600

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,35 @@
+import countryFacts from "../api/countryData.json";
+
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here Are the Interesting Facts
+        <br />
+        we're proud of
+      </h2>
+
+      <div className="gradient-cards">
+        {countryFacts.map((country) => {
+          const { id, countryName, capital, population, interestingFact } = country;
+          return (
+            <div className="card" key={id}>
+            <div className="container-card bg-blue-box">
+              <p className="card-title">{countryName}</p>
+              <p>
+                <span className="card-description">Population:</span>
+                {population}    
+              </p>
+              <p>
+                <span className="card-description">Interesting Fact:</span>
+                {interestingFact}
+              </p>
+            </div>
+            </div>
+          );
+        }
+        )}
+      </  div>
+    </section>
+  );
 };
