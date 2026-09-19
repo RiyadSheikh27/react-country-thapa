1cff9df7c29db374658349cdb7a3e67e544bce80
MD. Fazle Rabbi Riyad <135745417+RiyadSheikh27@users.noreply.github.com>
2026-09-19 23:22:20 +0600

    footer and contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,35 @@
 export const Contact = () => {
-  return <div>Contact</div>;
+  const handleFormSubmit = (formData) => {
+    console.log(formData);
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
+  return (
+    <section className="section-contact">
+      <h2 className='container-title'>Contact Us</h2>
+
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input type="text"
+            className="form-control"
+            required autoComplete="off"
+            placeholder='Enter your name'
+            name="username" />
+          <input type="email"
+            className="form-control"
+            required autoComplete="off"
+            placeholder='Enter your email'
+            name="email" />
+          <textarea className="form-control"
+            name="message"
+            required autoComplete="off"
+            placeholder='Enter your message'
+            rows="10"></textarea>
+
+          <button type='submit' value="send">Send</button>
+        </form>
+      </div>
+
+    </section>
+  )
 };
